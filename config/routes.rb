fd7bdc9fc306a93f1827620ae0f816b6e56ade0d
jay <jasanijay8@gmail.com>
2024-06-04 16:28:48 +0530

    sign in and sign up pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
   get "about", to: "about#index"
+
   get "sign_up", to: "registration#new"
   post "sign_up", to: "registration#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
   root to: "main#index"
 
 end
