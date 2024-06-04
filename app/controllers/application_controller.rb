09629789e8510b68529fa48b35f06df99508fcad
jay <jasanijay8@gmail.com>
2024-06-04 17:42:38 +0530

    route protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :set_current_user
-  
+  before_action :authenticate_user!
+
   def set_current_user
     if session[:user_id]
       Current.user = User.find_by(id: session[:user_id])
     end
   end
+
+  private
+  def authenticate_user!
+    redirect_to sign_in_path unless Current.user 
+  end
+
 end
